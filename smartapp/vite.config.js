edfc87b46b770778eba3281093c2573a4127d2ea
vigneshramraj <153055884+vigneshramraj@users.noreply.github.com>
2025-08-15 22:04:41 +0530

    base path in vite

--- smartapp/vite.config.js
+++ smartapp/vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,9 +7,5 @@ export default defineConfig({
     historyApiFallback: true,
   },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: "/",
 });
